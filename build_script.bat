2f0140f5505367dd472eff90c3e1686446ad97d0
Maxim <44000790+DavMd@users.noreply.github.com>
2024-05-30 13:53:48 +0300

    Update build_script.bat

--- build_script.bat
+++ build_script.bat
@@ -1,8 +1,10 @@
 @echo off
 REM Остановить выполнение скрипта при ошибке
 setlocal enabledelayedexpansion
+REM Создание директории для сборки
+if not exist "build" mkdir build
 REM Установить зависимости
 go mod tidy
 REM Сборка проекта
-go build -o InternProj ./...
+go build -o build/myapp ./...
 echo Сборка завершена
