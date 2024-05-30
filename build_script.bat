893ba748d6580c76d55e896b2b8b5706bb31e185
Maxim <counterggg@gmail.com>
2024-05-30 14:09:18 +0300

    Ref

--- build_script.bat
+++ build_script.bat
@@ -1,8 +1,5 @@
 @echo off
-REM Остановить выполнение скрипта при ошибке
 setlocal enabledelayedexpansion
-REM Установить зависимости
 go mod tidy
-REM Сборка проекта
 go build -o build ./...
 echo Сборка завершена
